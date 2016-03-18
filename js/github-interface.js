cf4e01beeba9d4c56ee8f19acb11d938ad42618e
Taylor Grohs <taylorgrohs@gmail.com>
2016-03-18 07:25:26 -0700

    add jquery get call for github

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,18 @@
-a
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function(){
+  $('#getName').submit(function(event){
+    event.preventDefault();
+    var name = $('#name').val();
+    $.get('https://api.github.com/users/' + name + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $('.repo').append('<li><img src='+ response[0].owner.avatar_url+'></li>');
+    for(var i = 0; i < response.length; i++)
+    {
+      $('.repo').append('<li><a href="https://github.com/'+name+'/'+response[i].name +'">'+response[i].full_name+ '</a></li>');
+    }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  });
+});
